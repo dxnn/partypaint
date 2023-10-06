60475ba77f0176299be49705c44700957dab486c
dann <gh@mrmuster.com>
2023-10-06 14:32:48 -0400

    floodfill, svg favicon, etc

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const el = document.getElementById.bind(document)
 const can = el('cancan')
-const ctx = can.getContext('2d')
+const ctx = can.getContext('2d', {willReadFrequently: true})
 let w = can.width
 let h = can.height
 let size = 5
@@ -15,15 +15,20 @@ can.addEventListener('mousemove', e => {
     ctx.fillStyle = '#000000'
   }
   if(paintmode === 1) {
-    ctx.fillStyle = filler(hue, 99, lightness, 100)
+    ctx.fillStyle = filler(hue, 90, lightness, 100)
+  }
+  if(paintmode === 3) {
+    ctx.fillStyle = filler(hue, 90, lightness, 99)
   }
   if(up === 0) {
-    if (paintmode === 2) {
+    if(paintmode === 2 || paintmode === 3) {
       ctx.clearRect(e.offsetX, e.offsetY, size, size)
-    } else {
+    }
+    if(paintmode !== 2) {
       ctx.fillRect(e.offsetX, e.offsetY, size, size)
     }
   }
+  console.log(ctx.fillStyle)
 })
 
 document.addEventListener('keydown', e => {
@@ -40,9 +45,9 @@ document.addEventListener('keydown', e => {
     hue = hue + 10
   }
   if(e.code === 'Space') {
-    paintmode = (paintmode + 1) % 3
+    paintmode = (paintmode + 1) % 4
   }
-  if(e.code === 'KeyP') {
+  if(e.code === 'KeyP' || e.code === 'KeyE') {
     party = (party + 1) % 2
   }
   if(e.code === 'KeyQ') {
@@ -53,7 +58,7 @@ document.addEventListener('keydown', e => {
 })
 
 function show_mode() {
-
+  el('mode').innerHTML = `${['draw','paint','erase','fill'][paintmode]} ${size}px ${hue}H 0S ${lightness}L ${up&&'invisible'||''} ${party&&'PARTY!!!'||''}`
 }
 
 function filler(h, s, l, a) { // 0 - 100
@@ -66,14 +71,51 @@ function partypaint() {
     const data = imageData.data
     for (let i = 0; i < data.length; i += 4) {
       if (data[i] || data[i + 1] || data[i + 2]) {
-        data[i] = (data[i] + 1) % 256         // red
+        data[i + 0] = (data[i + 0] + 1) % 256 // red
         data[i + 1] = (data[i + 1] + 1) % 256 // green
         data[i + 2] = (data[i + 2] + 1) % 256 // blue
       }
     }
+
     ctx.putImageData(imageData, 0, 0)
   }
 }
+
+function floodfill() {
+  const imageData = ctx.getImageData(0, 0, w, h)
+  const data = imageData.data
+  for (let i = 0; i < data.length; i += 4) {
+    if (data[i+3] === 252) {
+      let rand = Math.floor(Math.random()*4)
+      let n = [i-w*4,i+4,i+w*4,i-4][rand]
+      if(data[n+3] === 0 || data[n+3] === 252) {
+        data[n + 0] = (data[i + 0] + 1) % 256 // red
+        data[n + 1] = (data[i + 1] + 1) % 256 // green
+        data[n + 2] = (data[i + 2] + 1) % 256 // blue
+        data[n + 3] = data[i + 3]
+      }
+      // if(1 > Math.random() * 10) {
+      //   data[n + 3] = 255
+      // }
+    }
+  }
+  ctx.putImageData(imageData, 0, 0)
+}
+
+function comp(f, g) {
+  return x => f(g(x))
+}
+
+function save() {
+  alert('save')
+}
+
+function load() {
+  alert('load')
+}
+
+// el('export').addEventListener('click', save)
+// el('import').addEventListener('change', load)
 
 function go(f) {
   requestAnimationFrame(e => {
@@ -82,4 +124,5 @@ function go(f) {
   })
 }
 
-go(partypaint)
+show_mode()
+go(comp(floodfill, partypaint))
