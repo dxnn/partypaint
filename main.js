5777e2b16f9d3070f0cc4dc01fee9e6203f991b2
dann <gh@mrmuster.com>
2023-10-11 17:01:59 -0400

    better brushes!

--- main.js
+++ main.js
@@ -5,23 +5,24 @@ let w = can.width
 let h = can.height
 let size = 5
 let party = 0
-let paintmode = 0
-let lightness = 50
+let bnum = 0
 let hue = 0
+let sat = 90
+let lig = 50
 let up = 0
 
 document.addEventListener('keydown', e => {
   if(e.code === 'ArrowUp' || e.code === 'KeyW') {
-    size = size + 1
+    brush?.upup?.()
   }
   if(e.code === 'ArrowDown' || e.code === 'KeyS') {
-    size = size - 1
+    brush?.down?.()
   }
   if(e.code === 'ArrowLeft' || e.code === 'KeyA') {
-    lightness = (lightness + 10) % 101
+    brush?.left?.()
   }
   if(e.code === 'ArrowRight' || e.code === 'KeyD') {
-    hue = hue + 10
+    brush?.righ?.()
   }
   if(e.code === 'Space') {
     up = (up + 1) % 2
@@ -30,38 +31,24 @@ document.addEventListener('keydown', e => {
     party = (party + 1) % 2
   }
   if(e.code === 'KeyQ') {
-    paintmode = rem(paintmode - 1, 4)
+    bnum = rem(bnum - 1, 4)
+    brush = brushes[bnum]
   }
   if(e.code === 'KeyE') {
-    paintmode = (paintmode + 1) % 4
+    bnum = (bnum + 1) % 4
+    brush = brushes[bnum]
   }
 
   show_mode()
 })
 
 can.addEventListener('mousemove', e => {
-  if(paintmode === 0) {
-    ctx.fillStyle = '#000000'
-  }
-  if(paintmode === 1) {
-    ctx.fillStyle = filler(hue, 90, lightness, 100)
-  }
-  if(paintmode === 3) {
-    ctx.fillStyle = filler(hue, 90, lightness, 99)
-  }
-  if(up === 0) {
-    if(paintmode === 2 || paintmode === 3) {
-      ctx.clearRect(e.offsetX, e.offsetY, size, size)
-    }
-    if(paintmode !== 2) {
-      ctx.fillRect(e.offsetX, e.offsetY, size, size)
-    }
-  }
-  console.log(ctx.fillStyle)
+  if(up === 0)
+    brush.pynt(e)
 })
 
 function show_mode() {
-  el('mode').innerHTML = `${['draw','paint','erase','fill'][paintmode]} ${size}px ${hue}H 0S ${lightness}L ${up&&'invisible'||''} ${party&&'PARTY!!!'||''}`
+  el('mode').innerHTML = `${up&&'invisible'||''} ${party&&'PARTY!!!'||''} ${['draw','paint','erase','fill'][bnum]} ${size}px ${hue}H ${sat}S ${lig}L`
 }
 
 function filler(h, s, l, a) { // 0 - 100
@@ -69,64 +56,72 @@ function filler(h, s, l, a) { // 0 - 100
 }
 
 function partypaint() {
-  if (party) {
+  if(party) {
     const imageData = ctx.getImageData(0, 0, w, h)
     const data = imageData.data
-    for (let i = 0; i < data.length; i += 4) {
-      if (data[i] || data[i + 1] || data[i + 2]) {
-        data[i + 0] = (data[i + 0] + 1) % 256 // red
-        data[i + 1] = (data[i + 1] + 1) % 256 // green
-        data[i + 2] = (data[i + 2] + 1) % 256 // blue
-      }
-
-      // floodwaves
-      if (data[i + 3] === 252) {
-        let rand = Math.floor(Math.random() * 4)
-        let n = [i - w * 4, i + 4, i + w * 4, i - 4][rand]
-        if (data[n + 3] === 0 || data[n + 3] === 252) {
-          data[n + 0] = (data[i + 0] + 1) % 256 // red
-          data[n + 1] = (data[i + 1] + 1) % 256 // green
-          data[n + 2] = (data[i + 2] + 1) % 256 // blue
-          data[n + 3] = data[i + 3]
-        }
-        // if(1 > Math.random() * 10) {
-        //   data[n + 3] = 255
-        // }
-      }
+    for(let i = 0; i < data.length; i += 4) {
+      for(let b = 0; b < brushes.length; b++)
+        brushes[b]?.pard?.(data, i) // optimize this... could do a compose over the loadout to get a single pard fun
     }
 
     ctx.putImageData(imageData, 0, 0)
   }
 }
 
-// function floodfill() {
-//   const imageData = ctx.getImageData(0, 0, w, h)
-//   const data = imageData.data
-//   for (let i = 0; i < data.length; i += 4) {
-//     if (data[i+3] === 252) {
-//       let rand = Math.floor(Math.random()*4)
-//       let n = [i-w*4,i+4,i+w*4,i-4][rand]
-//       if(data[n+3] === 0 || data[n+3] === 252) {
-//         data[n + 0] = (data[i + 0] + 1) % 256 // red
-//         data[n + 1] = (data[i + 1] + 1) % 256 // green
-//         data[n + 2] = (data[i + 2] + 1) % 256 // blue
-//         data[n + 3] = data[i + 3]
-//       }
-//       // if(1 > Math.random() * 10) {
-//       //   data[n + 3] = 255
-//       // }
-//     }
-//   }
-//   ctx.putImageData(imageData, 0, 0)
-// }
+let brushes = [
+  { 'name': 'basic black'
+  , 'upup': _ => size += 1
+  , 'down': _ => size -= 1
+  , 'pynt': e => {ctx.fillStyle = '#000000'; fill(e)}
+  },
+  { 'name': 'pard paint'
+  , 'upup': _ => size += 1
+  , 'down': _ => size -= 1
+  , 'left': _ => hue = (hue + 10) % 101
+  , 'righ': _ => lig = (lig + 10) % 101
+  , 'pynt': e => {ctx.fillStyle = filler(hue, 90, lig, 100); fill(e)}
+  , 'pard': (data, i) => {
+              if (data[i] || data[i + 1] || data[i + 2]) {
+                data[i + 0] = (data[i + 0] + 1) % 256 // red
+                data[i + 1] = (data[i + 1] + 1) % 256 // green
+                data[i + 2] = (data[i + 2] + 1) % 256 // blue
+              }
+            }
+  },
+  { 'name': 'eraser'
+  , 'upup': _ => size += 1
+  , 'down': _ => size -= 1
+  , 'pynt': e => clear(e)
+  },
+  { 'name': 'floodwaves'
+  , 'upup': _ => size += 1
+  , 'down': _ => size -= 1
+  , 'left': _ => hue = (hue + 10) % 101
+  , 'righ': _ => lig = (lig + 10) % 101
+  , 'pynt': e => {ctx.fillStyle = filler(hue, 90, lig, 99); clear(e); fill(e)}
+  , 'pard': (data, i) => {
+              if (data[i + 3] === 252) {
+                let rand = Math.floor(Math.random() * 4)
+                let n = [i - w * 4, i + 4, i + w * 4, i - 4][rand]
+                if (data[n + 3] === 0 || data[n + 3] === 252) {
+                  data[n + 0] = (data[i + 0] + 1) % 256 // red
+                  data[n + 1] = (data[i + 1] + 1) % 256 // green
+                  data[n + 2] = (data[i + 2] + 1) % 256 // blue
+                  data[n + 3] = data[i + 3]
+                }
+              }
+      //   // if(1 > Math.random() * 10) {
+      //   //   data[n + 3] = 255
+      //   // }
+            }
+  },
+]
+let brush = brushes[0] // make_brush(brushes[0])
 
-function comp(f, g) {
-  return x => f(g(x))
-}
-
-function rem(n, r) {
-  return (n % r + r) % r
-}
+function noop() {}
+function rem(n, r) { return (n % r + r) % r }
+function fill(e) {ctx.fillRect(e.offsetX, e.offsetY, size, size)}
+function clear(e) {ctx.clearRect(e.offsetX, e.offsetY, size, size)}
 
 function save() {
   can.toBlob(function(blob) {
@@ -152,15 +147,12 @@ function save() {
 
 
 
-async function load(t) {
+function load(t) {
   let file = t.srcElement.files?.[0]
   let img = new Image()
   img.src = URL.createObjectURL(file)
   img.onload = _ => ctx.drawImage(img, 0, 0)
 }
-
-el('export').addEventListener('click', save)
-el('import').addEventListener('change', load)
 
 function go(f) {
   requestAnimationFrame(e => {
@@ -169,6 +161,8 @@ function go(f) {
   })
 }
 
+el('export').addEventListener('click', save)
+el('import').addEventListener('change', load)
+
 show_mode()
-// go(comp(floodfill, partypaint))
 go(partypaint)
