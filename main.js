1b8f4198cbf775ab971422e450f07642e95070db
dann <gh@mrmuster.com>
2023-11-10 17:51:03 -0500

    better brush loading

--- main.js
+++ main.js
@@ -14,15 +14,11 @@ const ctx = can.getContext('2d', {willReadFrequently: true})
 const w = can.width
 const h = can.height
 let types = new Uint8Array(w*h)
-let vals = {party: 1, lift: 0}
-let bnum = 0
+let vals = {party: 1, lift: 0, bnum: 0}
 let keymap = {}
 let loadout = []
 let brush
 
-// hey, start here!
-// - save/load full brushes!
-// - paint the brush!
 // - maybe tweak floodwaves so it's like it used to be?
 // - toda file connection...
 // - noisemaker brush that just randomly changes pixels...
@@ -40,8 +36,7 @@ let brush
 // surprise me brush
 // sandbags + sandflood brushes
 
-// when you save the loadout in the png save the current vals also
-// if we allow arbitrary brushes in the loadout that opens up security issues:
+// if we allow arbitrary brushes in the loadout that opens up security issues...
 // - once an image is loaded you have to reload the page to load a new one (to prevent interface highjacking)
 // - no localstorage, no twin access creds, etc
 // - keep it pure and simple, have a different interface for social aspects, load the viewer as a new window
@@ -58,7 +53,7 @@ let brush
 //        and much faster than the interleaved version, but it loses the compositional style of pixel-by-pixel brush stacking...
 
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', e => {                // menuing and D-pad
   keymap[e.code] = 1
 
   if(keymap['Space']) {
@@ -68,23 +63,23 @@ document.addEventListener('keydown', e => {
     vals.party = (vals.party + 1) % 2
   }
   if(keymap['KeyQ']) {
-    bnum = rem(bnum - 1, loadout.length)
-    brush = loadout[bnum]
+    vals.bnum = rem(vals.bnum - 1, loadout.length)
+    brush = loadout[vals.bnum]
   }
   if(keymap['KeyE']) {
-    bnum = (bnum + 1) % loadout.length
-    brush = loadout[bnum]
+    vals.bnum = (vals.bnum + 1) % loadout.length
+    brush = loadout[vals.bnum]
   }
 
   if(['Space', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.code))
     e.preventDefault()
 })
 
-document.addEventListener('keyup', e => {
+document.addEventListener('keyup', e => {                  // clear D-pad
   keymap[e.code] = 0
 })
 
-function keystuff() {
+function keystuff() {                                      // activate D-pad
   if(keymap['ArrowUp'] || keymap['KeyW']) {
     brush?.upup?.(vals)
   }
@@ -119,26 +114,27 @@ function partypaint() {
   }
 }
 
-function noop() {}
-function par(f, g) {return (a,b,c,d) => {f(a,b,c,d); g(a,b,c,d)}} // faster than variadic version... /shrug
-function rem(n, r) { return (n % r + r) % r }
-function rand(n) {return Math.floor(Math.random() * n)}
-function clear(v) {ctx.clearRect(v.x, v.y, v.syz, v.syz)}
-function fillRect(x, y, sw, sh, type) {
-  ctx.fillRect(x, y, sw, sh)
-  for(let i=y; i<y+sh; i++)
-    for(let j=x; j<x+sw; j++)
-      types[i*w + j] = type
-}
-
-function filler(h, s, l, a) { // 0 - 100, 0-255
-  return `hsl(${h * 3.6}, ${s}%, ${l}%, ${a/255})`
-}
-
-
 function show_brushes() {
   let sel = el('options')
   sel.innerHTML = '<option>Add a brush</option>' + brushes.map(b => `<option>${b.name}</option>`).join('')
+}
+
+function show_loadout() {
+  el('loaded').innerHTML = ''
+
+  if(!loadout.length)
+    loadout = brushes.slice(0,5)
+
+  loadout.forEach((brush, i) => {
+    if(brush.vals)
+      vals = {...brush.vals, ...vals}
+    el('loaded').innerHTML += `<span class="loaded" fwump="${i}" style="color: hsl(${[...brush.name].map(c=>c.charCodeAt(0)-90).reduce((acc,n)=>acc+n*7,0)}, 80%, 50%)">${brush.name}</span>`
+  })
+
+  if(!brush)
+    brush = loadout[0]
+
+  show_mode()
 }
 
 function show_mode() {
@@ -148,79 +144,22 @@ function show_mode() {
       el('mode').innerHTML += `<span class="${Object.entries(trick(brush)).reduce((acc, [key,v]) => v?.includes?.(k) ? acc + ' ' + key : acc, '')}">${vals[k]}${k}</span> `
 }
 
-function set_loadout() {
-  el('loaded').innerHTML = ''
-
-  if(!loadout.length)
-    loadout = brushes.slice(0,5)
-
-  for(let b = 0; b < loadout.length; b++) {
-    let brush = loadout[b]
-    if(brush.vals)
-      vals = {...brush.vals, ...vals}
-    el('loaded').innerHTML += `<span class="loaded" fwump="${b}" style="color: hsl(${[...brush.name].map(c=>c.charCodeAt(0)-90).reduce((acc,n)=>acc+n*7,0)}, 80%, 50%)">${brush.name}</span>`
-  }
-
-  if(!brush)
-    brush = loadout[0]
-
-  show_mode()
-}
-
-function add_brush(e) {
+function add_to_loadout(e) {
   let index = +el('options').selectedIndex - 1
   loadout.push(brushes[index])
-  set_loadout()
+  show_loadout()
   el('options').selectedIndex = 0
   el('options').blur()
 }
 
-function rm_brush(e) {
+function rm_from_loadout(e) {
   if(e.target.className !== 'loaded') return
   let i = +e.target.attributes.fwump?.value
   loadout.splice(i,1)
-  set_loadout()
+  show_loadout()
 }
 
-
-function chunker(arrayBuffer) {
-  let arr = new Uint8Array(arrayBuffer)
-  let cs = getChunks(arr)
-  return cs
-}
-
-function add_my_chunks(cs) {
-  let iend = cs.pop()
-  // THINK: consider packing types into a png to compact it...
-  let types_chunk = {chunkType: 'prDy', data: types}
-  let brush_chunk = {chunkType: 'paRd', data: slam_obj({vals, bnum, loadout: munge_loadout(loadout)})}
-  cs.push(types_chunk)
-  cs.push(brush_chunk)
-  cs.push(iend)
-  return cs
-}
-
-function munge_loadout(l) {
-  let bnames = brushes.map(b => b.name)
-  return l.map(b => bnames.includes(b.name) ? b.name : trick(b))
-}
-
-function unmunge_loadout(l) {
-  let bnames = brushes.map(b => b.name)
-  return l.map(b => brushes[bnames.indexOf(b)] || untrick(b))
-}
-
-function slam_obj(o) {
-  return JSON.stringify(o).split('').map(c=>c.charCodeAt())
-}
-
-function trick(o) {
-  return Object.entries(o).map(p => typeof p[1] === 'function' ? [p[0], ''+p[1]] : p).reduce((acc, [k, v])=> {acc[k]=v; return acc}, {})
-}
-
-function untrick(o) {
-  return Object.entries(o).map(p => typeof p[1] === 'function' ? [p[0], ''+p[1]] : p).reduce((acc, [k, v])=> {acc[k]=v; return acc}, {})
-}
+// SAVE & LOAD
 
 function save() {
   can.toBlob(async function(blob) {
@@ -241,6 +180,7 @@ async function load(t) {
   ctx.clearRect(0, 0, w, h)
   let img = new Image()
   img.src = URL.createObjectURL(file)
+  img.onload = _ => ctx.drawImage(img, 0, 0)
 
   // import types
   let arr = new Uint8Array(await file.arrayBuffer())
@@ -251,34 +191,105 @@ async function load(t) {
 
   // import vals, bnum and loadout
   let brush_chunk = find_first_chunk(cs, 'paRd')
-  let pickles = unslam(brush_chunk?.data)
+  let pickles = unslam_obj(brush_chunk?.data)
   if (pickles) {
     vals = pickles.vals
-    bnum = pickles.bnum
+    vals.bnum ||= pickles.bnum // TODO: remove this
     loadout = unmunge_loadout(pickles.loadout)
-    brush = loadout[bnum]
+    brush = loadout[vals.bnum]
     // TODO: extend brushes if new names, so you can get it back if you accidentally delete it
   }
 
   show_brushes()
-  set_loadout()
-  show_mode()
-
-  img.onload = _ => ctx.drawImage(img, 0, 0)
+  show_loadout()
   el('import').blur()
 }
 
-function find_first_chunk(cs, str) {
-  return cs.find(c => c.chunkType === str)
+function chunker(arrayBuffer) {
+  let arr = new Uint8Array(arrayBuffer)
+  let cs = getChunks(arr)
+  return cs
 }
 
-function unslam(ns) {
+function add_my_chunks(cs) {
+  let iend = cs.pop()
+  // THINK: consider packing types into a png to compact it...
+  let types_chunk = {chunkType: 'prDy', data: types}
+  let brush_chunk = {chunkType: 'paRd', data: slam_obj({vals, loadout: munge_loadout(loadout)})}
+  cs.push(types_chunk)
+  cs.push(brush_chunk)
+  cs.push(iend)
+  return cs
+}
+
+function munge_loadout(l) {
+  let bnames = brushes.map(b => b.name)
+  return l.map(b => trick(b))
+  // return l.map(b => bnames.includes(b.name) ? b.name : trick(b))
+}
+
+function unmunge_loadout(l) {
+  let bnames = brushes.map(b => b.name)
+  return l.map(b => untrick(b))
+  // return l.map(b => brushes[bnames.indexOf(b)] || untrick(b))
+}
+
+function slam_obj(o) {
+  return JSON.stringify(o).split('').map(c=>c.charCodeAt())
+}
+
+function unslam_obj(ns) {
   try {
     return JSON.parse(Array.from(ns).map(n=>String.fromCharCode(n)).join(''))
   } catch(e) {
     return null // {error: 'JSON, of course'}
   }
 }
+
+function trick(o) {
+  return Object.entries(o).map(p => typeof p[1] === 'function' ? [p[0], ''+p[1]] : p).reduce((acc, [k, v])=> {acc[k]=v; return acc}, {})
+}
+
+function untrick(o) {
+  return Object.entries(o).map(p => p[1]?.includes?.('=>') ? [p[0], new Function('return ' + p[1])()] : p).reduce((acc, [k, v])=> {acc[k]=v; return acc}, {})
+}
+
+function find_first_chunk(cs, str) {
+  return cs.find(c => c.chunkType === str)
+}
+
+// HELPERS
+
+function noop() {}
+function par(f, g) {return (a,b,c,d) => {f(a,b,c,d); g(a,b,c,d)}} // faster than variadic version... /shrug
+function rem(n, r) { return (n % r + r) % r }
+function rand(n) {return Math.floor(Math.random() * n)}
+function clear(v) {ctx.clearRect(v.x, v.y, v.syz, v.syz)}
+function fillRect(x, y, sw, sh, type) {
+  ctx.fillRect(x, y, sw, sh)
+  for(let i=y; i<y+sh; i++)
+    for(let j=x; j<x+sw; j++)
+      types[i*w + j] = type
+}
+
+function filler(h, s, l, a) { // 0 - 100, 0-255
+  return `hsl(${h * 3.6}, ${s}%, ${l}%, ${a/255})`
+}
+
+function timing() {
+  let now = performance.now()
+  partypaint()
+  console.log(performance.now() - now)
+}
+
+function add_brush(b) {
+  brushes.push(b)
+  loadout.push(b)
+  show_brushes()
+  show_loadout()
+}
+
+// BOOTSTRAP
 
 function go(f) {
   requestAnimationFrame(t => {
@@ -288,22 +299,14 @@ function go(f) {
 }
 
 function init() {
-  set_loadout()
   show_brushes()
-  show_mode()
+  show_loadout()
   go(par(partypaint, keystuff))
 }
 
 el('export').addEventListener('click', save)
 el('import').addEventListener('change', load)
-el('options').addEventListener('change', add_brush)
-el('loaded').addEventListener('click', rm_brush)
+el('options').addEventListener('change', add_to_loadout)
+el('loaded').addEventListener('click', rm_from_loadout)
 
 init()
-
-
-function timing() {
-  let now = performance.now()
-  partypaint()
-  console.log(performance.now() - now)
-}
